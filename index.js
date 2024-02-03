10535d3e2c94c22d31e23c7c27f2e20430e2aeb2
Isiaka Bhadmus <swiftnetuk@icloud.com>
2024-02-03 17:43:15 +0000

    Added:questions array to index.js

--- index.js
+++ index.js
@@ -5,7 +5,76 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message:'What is your project title',
+    },
 
+    {
+        type: 'input',
+        name: 'description',
+        message:'Write a brief description of your project',
+    },
+
+    {
+        type: 'input',
+        name: 'installation',
+        message:'Provide installation instructions',
+    },
+
+    {
+        type: 'input',
+        name: 'usage',
+        message:'Provide usage information',
+    },
+
+
+    {
+        type: 'input',
+        name: 'title',
+        message:'What is your project title',
+    },
+
+    {
+
+        type: 'list',
+        name: 'license',
+        message:'Choose a license for your application',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message:'What is your project title',
+    },
+
+    {
+        type: 'input',
+        name: 'contributing',
+        message:'Provide contribution guidelines:',
+    },
+
+    {
+        type: 'input',
+        name: 'tests',
+        message:'Provide test instructions',
+    },
+
+    {
+
+        type: 'input',
+        name: 'github',
+        message:'Enter your github username',
+    },
+
+    {
+        type: 'input',
+        name: 'email',
+        message:'Enter your email:',
+    },
+    
 ];
 
 // function to write README file
