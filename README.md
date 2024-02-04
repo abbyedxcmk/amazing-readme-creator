f481f047d07034eb72fc407ee3519580bf0f20b0
Isiaka Bhadmus <swiftnetuk@icloud.com>
2024-02-04 09:41:14 +0000

    Added: TOC md string to the readme

--- README.md
+++ README.md
@@ -1,23 +1,37 @@
-# ed
+# SWIFT SNL
 
-  ## Description
-  ed
 
-  ## Installation
-  ed
+## Table of Contents
 
-  ## Usage
-  ed
+[*SWIFT SNL*](#swift-snl)
+1. [Table of Contents](#table-of-contents)
+2. [Description](#description)
+3. [Installation](#installation)
+4. [Usage](#usage)
+5. [License](#license)
+6. [Contributing](#contributing)
+7. [Tests](#tests)
+8. [Questions](#questions)
+<hr>
 
-  ## License
-  MIT
 
-  ## Contributing
-  ed
+## Description
+SWIFTNET LINK
 
-  ## Tests
-  ed
+## Installation
+./saa_snl-install
 
-  ## Questions
-  If you have any questions or need to contact me about the code, you can do that through my GitHub Repository [github.com/tomdu3](https://github.com/tomdu3), or you can write to my email: ab@me.com
-  
+## Usage
+please change permissions on oradism file
+
+## License
+Apache 2.0
+
+## Contributing
+this software is developed by Abby NOT SWIFT! you pay me not them
+
+## Tests
+enter MT and we translate to MX ( for a fee)
+
+## Questions
+If you have any questions or need to contact me about the code, you can do that through my GitHub Repository [github.com/abbyedxcmk](https://github.com/abbyedxcmk), or you can write to my email: abiola@internationalbankingacademy.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,29 +1,49 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+
+  // generate a Table of Contents MD string
+  // for the title - we split the string and join it again with dashes in order to make a MD link for the TOC
+  const TOC = 
+`
+## Table of Contents
+
+[*${data.title}*](#${data.title.split(' ').join('-').toLowerCase()})
+1. [Table of Contents](#table-of-contents)
+2. [Description](#description)
+3. [Installation](#installation)
+4. [Usage](#usage)
+5. [License](#license)
+6. [Contributing](#contributing)
+7. [Tests](#tests)
+8. [Questions](#questions)
+<hr>
+`
   // make outputMarkdown variable to contain MD syntax string for output
   const outputMarkdown = `# ${data.title}
 
-  ## Description
-  ${data.description}
+${TOC}
 
-  ## Installation
-  ${data.installation}
+## Description
+${data.description}
 
-  ## Usage
-  ${data.usage}
+## Installation
+${data.installation}
 
-  ## License
-  ${data.license}
+## Usage
+${data.usage}
 
-  ## Contributing
-  ${data.contributing}
+## License
+${data.license}
 
-  ## Tests
-  ${data.tests}
+## Contributing
+${data.contributing}
 
-  ## Questions
-  If you have any questions or need to contact me about the code, you can do that through my GitHub Repository [github.com/${data.github}](https://github.com/${data.github}), or you can write to my email: ${data.email}
-  `;
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions or need to contact me about the code, you can do that through my GitHub Repository [github.com/${data.github}](https://github.com/${data.github}), or you can write to my email: ${data.email}
+`;
   return outputMarkdown;
 }
 
